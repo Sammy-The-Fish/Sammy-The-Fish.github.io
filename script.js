e18f056125911721720e608e786e3b772ab6c99f
Sammy-The-Fish <samuellnewman07@gmail.com>
2024-03-18 23:26:40 +0000

    Adding animated switching

--- script.js
+++ script.js
@@ -1,34 +1,73 @@
 
 
-let id = null;
-const MainTitle = document.getElementById("MainTitle");
-let pos = 0;
-clearInterval(id);
-id = setInterval(frame, 0.1);
 
-const HomeDiv = document.getElementById("HomeDiv")
+var CurrentPosition = 0;
+var TargetPosition
 
-
-function frame() {
-  if (pos == 104) {
-    clearInterval(id);
-  } else {
-    pos++;
-    MainTitle.style.fontSize = pos + 'px';
-    MainTitle.style.fontSize = pos + 'px';
-  }
+for(i =0; i<1; i+=0.01){
+    console.log(easeInOutCubic(i))
 }
 
 
-function TransProject() {
+function TransHome(){
+    TargetPosition = 0
+    Transition()
+}
+
+
+
+function TransProject(){
+    TargetPosition = 1
+    Transition()
+}
+
+
+function TransContact(){
+    console.log("test")
+    alert("Fuck off, not made yet 😡")
+}
+
+function Transition() {
+    console.log("TARGET: " + TargetPosition + " CURRENT: " + CurrentPosition)
+    if (CurrentPosition == TargetPosition){
+        return
+    }
+    const ProjectDiv = document.getElementById("ProjectDiv")
+    const HomeDiv = document.getElementById("HomeDiv")
+    console.log("HELLOW :3")
+    let id = null;
+    let pos = 0;
+    id = setInterval(frame, 1);
+
+    if (TargetPosition > CurrentPosition){
+        anim_step = -1
+        target = -100
+        start = 0
+    }else{
+        anim_step = +1
+        target = 0
+        start = -100
+    }
+
+    let time = 0
     function frame() {
-        if (pos == 104) {
-          clearInterval(id);
+        if (time == 100) {
+            clearInterval(id);
+            CurrentPosition = TargetPosition
         } else {
-          pos++;
-          MainTitle.style.fontSize = pos + 'px';
-          MainTitle.style.fontSize = pos + 'px';
+            time++
+            pos = ((anim_step * easeInOutCubic(time/100)) * 100) + start
+            ProjectPos = pos + 100
+            HomeDiv.style.left = pos + '%';
+            ProjectDiv.style.left = ProjectPos + '%'
         }
       }
       
 }
+
+
+
+
+function easeInOutCubic(x) {
+    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+    }
