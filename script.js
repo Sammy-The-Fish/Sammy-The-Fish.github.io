6a65b43624774338870e3606ac3e2f2f81b0dff5
Sammy-The-Fish <samuellnewman07@gmail.com>
2024-03-18 20:27:22 +0000

    Homepage basics made

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// let id = null;
+// const MainTitle = document.getElementById("MainTitle");
+// let pos = 0;
+// clearInterval(id);
+// id = setInterval(frame, 0.1);
+
+
+
+// function frame() {
+//   if (pos == 104) {
+//     clearInterval(id);
+//   } else {
+//     pos++;
+//     MainTitle.style.fontSize = pos + 'px';
+//     MainTitle.style.fontSize = pos + 'px';
+//   }
+// }
