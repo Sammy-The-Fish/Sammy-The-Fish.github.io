76e6baec54f16186a984dec78d875540dc79c564
Sammy-The-Fish <samuellnewman07@gmail.com>
2024-03-18 22:07:54 +0000

    Continuing developing

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 
 
-// let id = null;
-// const MainTitle = document.getElementById("MainTitle");
-// let pos = 0;
-// clearInterval(id);
-// id = setInterval(frame, 0.1);
+let id = null;
+const MainTitle = document.getElementById("MainTitle");
+let pos = 0;
+clearInterval(id);
+id = setInterval(frame, 0.1);
+
+const HomeDiv = document.getElementById("HomeDiv")
 
 
+function frame() {
+  if (pos == 104) {
+    clearInterval(id);
+  } else {
+    pos++;
+    MainTitle.style.fontSize = pos + 'px';
+    MainTitle.style.fontSize = pos + 'px';
+  }
+}
 
-// function frame() {
-//   if (pos == 104) {
-//     clearInterval(id);
-//   } else {
-//     pos++;
-//     MainTitle.style.fontSize = pos + 'px';
-//     MainTitle.style.fontSize = pos + 'px';
-//   }
-// }
+
+function TransProject() {
+    function frame() {
+        if (pos == 104) {
+          clearInterval(id);
+        } else {
+          pos++;
+          MainTitle.style.fontSize = pos + 'px';
+          MainTitle.style.fontSize = pos + 'px';
+        }
+      }
+      
+}
