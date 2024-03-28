487c3ec2c2147ae7a229d538fceab9acbe60d788
sammy-the-fish <samuellnewman07@gmail.com>
2024-03-28 17:15:44 +0000

    small tweaks

---
+++ LeverSim2/anouncement.js
@@ -0,0 +1,10 @@
+// const Panel = document.getElementById("Moving")
+
+
+// ScrollPos = document.documentElement.scrollTop || document.body.scrollTop
+
+// setInterval(Scrolling, 5)
+
+// function Scrolling(){
+//     Panel.style.transform = "translateX(" + ScrollPos + ")"
+// }
