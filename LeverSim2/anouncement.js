8bf1cef55108b1af07f385623f0f7d9438a84a03
Sammy-The-Fish <samuellnewman07@gmail.com>
2024-03-29 16:11:23 +0000

    finalised Lever sim 2 anouncement page

--- LeverSim2/anouncement.js
+++ LeverSim2/anouncement.js
@@ -1,10 +1,74 @@
-// const Panel = document.getElementById("Moving")
+const Panel = document.getElementById("RestOfWebsite")
+const Title = document.getElementById("MainTitle")
+const background = document.getElementById("FadeIn")
 
 
-// ScrollPos = document.documentElement.scrollTop || document.body.scrollTop
 
-// setInterval(Scrolling, 5)
+window.onload = fadeIn()
+entering = true
+function fadeIn(){
+    entering = true
+    console.log("test")
+    id=setInterval(Frame, 15, 100)
+    time = 0
 
-// function Scrolling(){
-//     Panel.style.transform = "translateX(" + ScrollPos + ")"
+    function Frame(length){
+        if (time == length){
+            entering = false
+            clearInterval(id)
+            return
+        }
+        time++
+        pos = time/length
+        let easepos = easeInCubic(pos)
+
+        background.style.backgroundImage = "radial-gradient(#00000000 , rgba(0,0,0, " + (100-(easepos * 100))+ "%) 0%)"
+    }
+}
+
+
+
+
+
+
+
+
+
+// function transition(){
+//     if(entering){
+//         return
+//     }
+//     time = 0
+//     id = setInterval(Frame, 20, 50)
+//     console.log("testing")
+//     function Frame(length){
+//         if (time == length){
+//             clearInterval(id)
+//         }else{
+//             console.log(time)
+//             time ++
+//             pos = time/length
+//             easepos = easeInOutExpo(pos)
+//             Title.style.top = (50-(easepos * 100)) + "%"
+//             Panel.style.marginTop = (100 - (easepos * 75))  + "%"
+//         }
+//     }
 // }
+
+
+
+
+function easeInOutExpo(x){
+    return x === 0
+      ? 0
+      : x === 1
+      ? 1
+      : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
+      : (2 - Math.pow(2, -20 * x + 10)) / 2;
+    
+    }
+
+    function easeInCubic(x){
+        return x * x * x;
+        
+        }
