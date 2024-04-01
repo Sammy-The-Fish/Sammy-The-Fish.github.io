6278acbf52b3b9faad58d263e466c02e24156500
Sammy-The-Fish <samuellnewman07@gmail.com>
2024-04-01 13:00:46 +0100

    Added prototype Lever sim 2

--- LeverSim2/Game/LeverSim2.js
+++ LeverSim2/Game/LeverSim2.js
@@ -6,8 +6,8 @@ window.onload = loadBackground()
 
 function loadBackground(){
     let string = ""
-    for (let i = 0; i<100; i++){
-        for(let j=0; j<100; j++){
+    for (let i = 0; i<50; i++){
+        for(let j=0; j<60; j++){
         string += " 2"
         }
         string += "<br>"
@@ -15,32 +15,123 @@ function loadBackground(){
     Numbers.innerHTML = string
 }
 
-
-function changeBackground(){
-    
-    id = setInterval(Frame, 50, 100, " 3")
+oldvalue = " 2"
+function changeBackground(newValue, color){
+    id2 = setInterval(BackgroundFrame, 25, 100, newValue, color)
+    console.log(id2)
     time = 0
-    function Frame(length, newValue){
+    function BackgroundFrame(length, newValue, color){
         if(time == length){
-            clearInterval(id)
+            oldvalue = " " + num
+            Pulling = false
+            console.log(id2)
+            clearInterval(id2)
+            console.log(id2)
             return
         }
-        console.log("running")
         time++
         let string = ""
-        for (let i = 0; i<100; i++){
-            for(let j=0; j<100; j++){
+        for (let i = 0; i<50; i++){
+            for(let j=0; j<60; j++){
                 if(j == (time - i)){
-                    string += "<span style = 'font-weight: bolder'>" + newValue + "</span>"
+                    string += "<span style = 'font-weight: bolder; color: " + color + "'>" + newValue + "</span>"
                 }else if(j <= (time - i)){
                     string += newValue
                 }else{
-                    string += " 2"
+                    string += oldvalue
                 }
             }
             string += "<br>"
         }
-        console.log(string)
         Numbers.innerHTML = string
+    }
+}
+
+
+
+const Lever = document.getElementById("Lever")
+const LeverText = document.getElementById("LeverText")
+const accent = document.querySelector(':root');
+
+
+
+Pulling = false
+stopped = false
+function CrankLever(profesional){
+
+    // console.log(oldvalue)
+    if (Pulling || stopped){
+        return
+    }
+    Pulling = true
+    num = Math.floor((Math.random() * 3) + 1)
+
+    time = 0
+    id = setInterval(Frame, 5)
+    function Frame(){
+        if(time == 320){
+            // prob = Math.random()
+            changeBackground(" 3", "yellow")
+            if(prob == 1){
+                Lever.style.filter = "hue-rotate(240deg)"
+                accent.style.setProperty('--accent', 'blue');
+                changeBackground(" 1", "blue")
+                num = 1;
+            }else if (prob == 2){
+                Lever.style.filter = "hue-rotate(120deg)"
+                accent.style.setProperty('--accent', 'green');
+                changeBackground(" 2", "green")
+                num = 2
+            }else if (prob == 3){
+                Lever.style.filter = "hue-rotate(60deg)"
+                accent.style.setProperty('--accent', 'yellow');
+                changeBackground(" 3", "yellow")
+                num = 3
+            }else if (prob == 4){
+                Lever.style.filter = "hue-rotate(0deg)"
+                accent.style.setProperty('--accent', 'red');
+                changeBackground(" 4", "red")
+                num = 4
+            }
+            LeverText.innerHTML = "<center>" + num + "</center>"
+            clearInterval(id)
+            return
+        }
+        let stageLength = 80
+        stage = Math.floor(time/stageLength)
+        time++
+        switch (stage){
+            case 0:
+                Lever.style.transform = "rotate(" + (time-45) + "deg)"
+                break
+            case 3:
+                let rot = time - 3 * stageLength
+                Lever.style.transform = "rotate(" + ((80-rot)-45) + "deg)"
+                break
+            default:
+                if (time % 15 == 0){
+                    prob = Math.ceil(Math.random() * 3)
+                    if(prob == 1){
+                        Lever.style.filter = "hue-rotate(240deg)"
+                        accent.style.setProperty('--accent', 'blue');
+                        num = 1;
+                    }else if (prob == 2){
+                        Lever.style.filter = "hue-rotate(120deg)"
+                        accent.style.setProperty('--accent', 'green');
+                        num = 2
+                    }else if (prob == 3){
+                        Lever.style.filter = "hue-rotate(60deg)"
+                        accent.style.setProperty('--accent', 'yellow');
+                        num = 3
+                    }else if (prob == 4){
+                        Lever.style.filter = "hue-rotate(0deg)"
+                        accent.style.setProperty('--accent', 'red');
+                        num = 4
+                    }
+                    LeverText.innerHTML = "<center>" + num + "</center>"
+            }
+            
+
+        }
     }
 }
